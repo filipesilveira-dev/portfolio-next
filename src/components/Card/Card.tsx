68c1c32cc1b8b830f1f64652fc056ac32fc3a5e5
Filipe <filipesilveira.dev@gmail.com>
2026-09-11 18:15:41 -0300

    style: estilização da seção de projetos

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,34 @@
-import style from "./Card.module.css"
-import Image from "next/image";
+import style from "./Card.module.css";
+import Image , {StaticImageData}from "next/image";
 import Link from "next/link";
 
-interface CardProps{
+interface CardProps {
   name: string;
   resume: string;
   tech: string[];
+  img: string | StaticImageData;
 }
 
-export default function Card({name, resume, tech}: CardProps) {
+export default function Card({ img, name, resume, tech }: CardProps) {
   return (
     <>
       <Link href="/projects" className={style.link_wrapper}>
-      
-        <Image className={style.card_img} src="https://placehold.co/135x90" width={135} height={90} alt="Miniatura do projeto" unoptimized/>
+        <Image
+          className={style.card_img}
+          src={img}
+          width={135}
+          height={90}
+          alt="Miniatura do projeto"
+          unoptimized
+        />
         <div className={style.project_info_container}>
-            <h3>{name}</h3>
-            <p>{resume}</p>
-            <div className={style.span_wrapper}>
-                {tech.map((t)=>(<span key={t}>{t}</span>))}
-            </div>
+          <h3>{name}</h3>
+          <p>{resume}</p>
+          <div className={style.span_wrapper}>
+            {tech.map((t) => (
+              <span key={t}>{t}</span>
+            ))}
+          </div>
         </div>
       </Link>
     </>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,30 +1,47 @@
 import Link from "next/link";
 import Card from "../Card/Card";
-import style from "./Projects.module.css"
-// nome, resumo, três tecnologias
-// import {projects} from "@/utils/projects"
+import style from "./Projects.module.css";
+import { projects } from "@/utils/projects";
+import Image from "next/image";
+// import arrow from "./arrow.svg"
 
-export interface Projeto{
+export interface Projeto {
   id: number;
+  img: string;
   name: string;
   resume: string;
   tech: string[];
 }
 
-const projetoMock = [{
-  id: 1,
-  name: "Balance Me",
-  resume: "Veja seu equilíbrio diário",
-  tech: ["React", "Typescript", "CSS Modules"],
-}];
+// const projetoMock = [
+//   {
+//     id: 1,
+//     name: "Balance Me",
+//     resume: "Veja seu equilíbrio diário",
+//     tech: ["React", "Typescript", "CSS Modules"],
+//   },
+// ];
 
 export default function Projects() {
   return (
-    <div className={style.projects_container}>
-      <h2>Projetos em destaque</h2>
-      <Link href="/projects">Ver todos</Link>
-      <Link href="/projects" />
-      {projetoMock.map((projeto)=>(<Card key={projeto.id} name={projeto.name} resume={projeto.resume} tech={projeto.tech}/>))}
+    <div className={style.projects_section}>
+      <div className={style.projets_section_title_wrapper}>
+        <h2>Projetos em destaque</h2>
+        <Link className={style.projects_link} href="/projects">
+          Ver todos{" "}
+          <Image src="/arrow.svg" width={20} height={20} alt="Imagem de seta" />
+        </Link>
+      </div>
+
+      {projects.map((project) => (
+        <Card
+          key={project.id}
+          img={project.imgM}
+          name={project.name}
+          resume={project.resume}
+          tech={project.tech}
+        />
+      ))}
     </div>
   );
 }
