4a4a8b2692009d54907d8f50aa7db21b12bbf36e
Filipe <filipesilveira.dev@gmail.com>
2026-09-16 15:40:03 -0300

    style: estilização de navegação nos Links da navbar e ajuste de altura dos cards no componente Projects.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,11 +7,29 @@ import Link from "next/link";
 import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 
+type NavLinkId =
+  "home" | "about" | "projects" | "skills" | "articles" | "contact";
+interface NavItem {
+  id: NavLinkId;
+  label: string;
+  path: string;
+}
+
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState<NavLinkId>("home");
+
+  const navItems: NavItem[] = [
+    { id: "home", label: "Início", path:"" },
+    { id: "about", label: "Sobre", path:"about" },
+    { id: "projects", label: "Projetos", path:"projects" },
+    { id: "skills", label: "Habilidades", path:"skills" },
+    { id: "articles", label: "Artigos", path:"articles" },
+    { id: "contact", label: "Contato", path:"contact" },
+  ];
 
   return (
-    <header className={`${style.header} ${isOpen? style.header_open: ""}`}>
+    <header className={`${style.header} ${isOpen ? style.header_open : ""}`}>
       <section className={style.symbols_container}>
         <Link href="/">
           <Image src="/logo.svg" alt="Imagem da logo" width={40} height={40} />
@@ -19,12 +37,36 @@ export function Header() {
 
         <nav className={style.nav_bar}>
           <ul className={style.nav_bar_list}>
-            <Link href="/" className={style.nav_bar_item}>Início</Link>
-            <Link href="/projects" className={style.nav_bar_item}>Projetos</Link>
-            <Link href="/about" className={style.nav_bar_item}>Sobre</Link>
-            <Link href="/skills" className={style.nav_bar_item}>Habilidades</Link>
-            <Link href="/articles" className={style.nav_bar_item}>Artigos</Link>
-            <Link href="/contact" className={style.nav_bar_item}>Contato</Link>
+            {navItems.map((item) => (
+              <Link
+                key={item.id}
+                href={`/${item.path}`}
+                className={`${style.nav_bar_item} ${isActive === item.id ? style.active: ""}`}
+                onClick={() => setIsActive(item.id)}
+              >
+                {item.label}
+              </Link>
+            ))}
+
+
+            {/* <Link href="/" className={style.nav_bar_item}>
+              Início
+            </Link>
+            <Link href="/projects" className={style.nav_bar_item}>
+              Projetos
+            </Link>
+            <Link href="/about" className={style.nav_bar_item}>
+              Sobre
+            </Link>
+            <Link href="/skills" className={style.nav_bar_item}>
+              Habilidades
+            </Link>
+            <Link href="/articles" className={style.nav_bar_item}>
+              Artigos
+            </Link>
+            <Link href="/contact" className={style.nav_bar_item}>
+              Contato
+            </Link> */}
           </ul>
         </nav>
 
@@ -40,7 +82,13 @@ export function Header() {
               height={24}
             />
           </button>
-          <Image src="/moon.svg" alt="Imagem de lua" width={36} height={36} className={style.toggle_symbol}/>
+          <Image
+            src="/moon.svg"
+            alt="Imagem de lua"
+            width={36}
+            height={36}
+            className={style.toggle_symbol}
+          />
         </div>
       </section>
 
