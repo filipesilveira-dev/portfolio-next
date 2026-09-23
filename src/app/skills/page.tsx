91231d7a984ea7d77a58c9960c1a19c0dc1c1a21
Filipe <filipesilveira.dev@gmail.com>
2026-09-23 13:22:29 -0300

    feat: implementação da página de habilidades para telas mobile

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,5 +1,50 @@
-export default function Skills(){
-    return(
-        <h2>Habilidades</h2>
-    )
+import Contact from "@/components/Contact/Contact";
+import style from "./page.module.css";
+import { skills } from "@/utils/skills";
+import Image from "next/image";
+
+export default function Skills() {
+  return (
+    <>
+      <div className={style.text_wrapper}>
+        <h2 className={style.title}>
+          <span>Habilidades</span>
+        </h2>
+        <p className={style.subtitle}>
+          Tecnologias e ferramentas que domino no desenvolvimento front-end.
+        </p>
+      </div>
+
+      {/* Tabela em telas menores */}
+      <ul className={style.list_container}>
+        {skills.map((skill) => (
+          <li key={skill.id} className={style.list_item_container}>
+            <Image
+              width={40}
+              height={40}
+              alt={`Logo de ${skill.id}`}
+              src={skill.img}
+              unoptimized
+            />
+            <div className={style.list_item_text_wrapper}>
+              <span>
+                <strong>{skill.name}</strong>
+              </span>
+              <div
+                className={`
+                    ${skill.skillLv === "Básico" ? style.basic : ""}
+                    ${skill.skillLv === "Intermediário" ? style.intermediate : ""}
+                    ${skill.skillLv === "Avançado" ? style.advanced : ""}
+                `}
+              >
+                {skill.skillLv}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <Contact />
+    </>
+  );
 }
--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,15 +1,28 @@
 // Arquivo que será consumido na página de tecnologia (pode ser reaproveitado no componente Tech eventualmente)
+import html from "../../public/tech_logos/html.png";
+import css from "../../public/tech_logos/css.png";
+import tailwind from "../../public/tech_logos/tailwind.png";
+import js from "../../public/tech_logos/js.png";
+import ts from "../../public/tech_logos/typescript.png";
+import node from "../../public/tech_logos/nodejs.png";
+import react from "../../public/tech_logos/react.png";
+import motion from "../../public/tech_logos/motion.png";
+import next from "../../public/tech_logos/Next.js.png";
+import vercel from "../../public/tech_logos/vercel.png";
+import git from "../../public/tech_logos/git.png";
+import github from "../../public/tech_logos/githubLight.png";
+
 export const skills = [
-  { id: "html", name: "HTML5", img: "", skillLv: "" },
-  { id: "css", name: "CSS3", img: "", skill: "" },
-  { id: "tailwind", name: "Tailwind CSS", img: "", skillLv: "" },
-  { id: "js", name: "JavaScript", img: "", skillLv: "" },
-  { id: "ts", name: "Typescript", img: "", skillLv: "" },
-  { id: "node", name: "Node Js", img: "", skillLv: "" },
-  { id: "react", name: "React", img: "", skillLv: "" },
-  { id: "motion", name: "Motion", img: "", skillLv: "" },
-  { id: "next", name: "Next.js", img: "", skillLv: "" },
-  { id: "vercel", name: "Vercel", img: "", skillLv: "" },
-  { id: "git", name: "Git", img: "", skillLv: "" },
-  { id: "github", name: "GitHub", img: "", skillLv: "" },
+  { id: "html", name: "HTML5", img: html, skillLv: "Intermediário" },
+  { id: "css", name: "CSS3", img: css, skillLv: "Intermediário" },
+  { id: "tailwind", name: "Tailwind CSS", img: tailwind, skillLv: "Básico" },
+  { id: "js", name: "JavaScript", img: js, skillLv: "Intermediário" },
+  { id: "ts", name: "Typescript", img: ts, skillLv: "Intermediário" },
+  { id: "node", name: "Node Js", img: node, skillLv: "Básico" },
+  { id: "react", name: "React", img: react, skillLv: "Avançado" },
+  { id: "motion", name: "Motion", img: motion, skillLv: "Básico" },
+  { id: "next", name: "Next.js", img: next, skillLv: "Intermediário" },
+  { id: "vercel", name: "Vercel", img: vercel, skillLv: "" },
+  { id: "git", name: "Git", img: git, skillLv: "Básico" },
+  { id: "github", name: "GitHub", img: github, skillLv: "" },
 ];
